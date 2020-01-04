9d18ecf37e8ed0da0c29a7c0dd9bd0123eaecdd5
Clemens Schmid <clemens@nevrome.de>
2020-01-04 21:50:30 +0100

    documentation and some new parameters for tessellate

--- R/tessellate.R
+++ R/tessellate.R
@@ -1,12 +1,35 @@
 #' tessellate
+#' 
+#' Command line utility wrapper for the \href{http://math.lbl.gov/voro++}{voro++} software library.
+#' 
+#' @param x data.frame with the input points described by four variables (named columns):
+#' \itemize{
+#'   \item id: id number that is passed to the output polygon (integer)
+#'   \item x: x-axis coordinate (numeric)
+#'   \item y: y-axis coordinate (numeric)
+#'   \item z: z-axis coordinate (numeric)
+#' }
+#' @param x_min minimum x-axis coordinate of the box. Default: min(x)
+#' @param x_max maximum x-axis coordinate of the box. Default: max(x)
+#' @param y_min minimum y-axis coordinate of the box. Default: min(y)
+#' @param y_max maximum y-axis coordinate of the box. Default: max(y)
+#' @param z_min minimum z-axis coordinate of the box. Default: min(z)
+#' @param z_max maximum z-axis coordinate of the box. Default: max(z)
+#' @param output_definition string that describes how the output file of voro++ should be structured.
+#' This is passed to the -c option of the command line interface. All possible customization options 
+#' are documented \href{http://math.lbl.gov/voro++/doc/custom.html}{here}. Default: "\%i§\%P§\%t"
+#' @param options string with additional options passed to voro++. All options are documented 
+#' \href{http://math.lbl.gov/voro++/doc/cmd.html}{here}. Default: "-v"
+#' @param voro_path system path to the voro++ executable. Default: "voro++"
 #'
-#' @param x 
-#'
-#' @return
+#' @return The raw, linewise output of voro++ in a character vector
 #' @export
-#'
-#' @examples
-tessellate <- function(x) {
+tessellate <- function(
+  x, 
+  x_min = NA, x_max = NA, y_min = NA, y_max = NA, z_min = NA, z_max = NA,
+  output_definition = "%i§%P§%t", options = "-v",
+  voro_path = "voro++"
+) {
 
   to_voro <- tempfile()
   from_voro <- paste0(to_voro, ".vol")
@@ -14,18 +37,19 @@ tessellate <- function(x) {
   readr::write_delim(x, path = to_voro, col_names = F)
   
   system(paste(
-    "voro++",
+    voro_path,
     # output string
-    "-c %i§%P§%t",
-    # verbose
-    "-v",
+    paste("-c", output_definition),
+    # additional options
+    options,
     # x_min x_max y_min y_max z_min z_max
-    min(x$x),
-    max(x$x),
-    min(x$y),
-    max(x$y),
-    min(x$z),
-    max(x$z),
+    ifelse(is.na(x_min), min(x$x), x_min),
+    ifelse(is.na(x_max), max(x$x), x_max),
+    ifelse(is.na(y_min), min(x$y), y_min),
+    ifelse(is.na(y_max), max(x$y), y_max),
+    ifelse(is.na(z_min), min(x$z), z_min),
+    ifelse(is.na(z_max), max(x$z), z_max),
+    # input file
     to_voro
   ))
   
