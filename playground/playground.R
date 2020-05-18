88e4d045beee791f5fe284f93d87f5798f0e1866
Clemens Schmid <clemens@nevrome.de>
2020-05-18 22:33:08 +0200

    further work on the general workflow for the voro temporal wiggle prediction

--- playground/playground.R
+++ playground/playground.R
@@ -29,43 +29,60 @@ c14 <- c14_cmr_unique %>%
     x = coords[,1], 
     y = coords[,2], 
     z = c14age * 1000, # rescaling of temporal data
-    material = material
+    lat = lat,
+    lon = lon
   )
 
+c14_merge <- c14 %>% dplyr::select(id, lat, lon)
 
+c14_list <- list(c14_1 = c14, c14_2 = c14)
 
-raw_voro_output <- bleiglas::tessellate(
-  c14[, c("id", "x", "y", "z")],
-  x_min = min(c14$x) - 150000, x_max = max(c14$x) + 150000, 
-  y_min = min(c14$y) - 150000, y_max = max(c14$y) + 150000
-)
-
-polygon_edges <- bleiglas::read_polygon_edges(raw_voro_output)
-
-polygon_edges %<>% dplyr::mutate(
-  z.a = z.a / 1000,
-  z.b = z.b / 1000
-)
-
-c14 %<>% dplyr::mutate(
-  z = z / 1000
-)
-
+#### prediction grid ####
 pred_grid <- expand.grid(
   x = seq(min(c(polygon_edges$x.a, polygon_edges$x.b)) + 1, max(c(polygon_edges$x.a, polygon_edges$x.b)) - 1, by = 50000),
   y = seq(min(c(polygon_edges$y.a, polygon_edges$y.b)) + 1, max(c(polygon_edges$y.a, polygon_edges$y.b)) - 1, by = 50000),
   z = seq(min(c(polygon_edges$z.a, polygon_edges$z.b)) + 1, max(c(polygon_edges$z.a, polygon_edges$z.b)) - 1, by = 200)
 )
+pred_grid$point_id <- 1:nrow(pred_grid)
 
-pred_grid$polygon_id <- bleiglas::attribute_grid_points(polygon_edges, pred_grid)
 
-spu <- pred_grid %>% data.table::merge.data.table(
-  c14 %>% dplyr::select(id, material), by.x = "polygon_id", by.y = "id"
-)
+res <- lapply(1:length(c14_list), function(c14_table_id) {
 
-ggplot() +
-  geom_point(
-    data = spu %>% dplyr::filter(z == 2611),
-    mapping = aes(x, y, color = material),
-    alpha = 0.5, size = 10
+  c14_table <- c14_list[[c14_table_id]]
+  
+  raw_voro_output <- bleiglas::tessellate(
+    c14_table[, c("id", "x", "y", "z")],
+    x_min = min(c14_table$x) - 150000, x_max = max(c14_table$x) + 150000, 
+    y_min = min(c14_table$y) - 150000, y_max = max(c14_table$y) + 150000, 
+    options = ""
   )
+  
+  polygon_edges <- bleiglas::read_polygon_edges(raw_voro_output)
+  
+  polygon_edges %<>% dplyr::mutate(
+    z.a = z.a / 1000,
+    z.b = z.b / 1000
+  )
+  
+  point_polygon <- data.table::data.table(
+    point_id = pred_grid$point_id,
+    id = bleiglas::attribute_grid_points(polygon_edges, pred_grid)
+  )
+  
+  spu <- point_polygon %>% data.table::merge.data.table(
+    c14_merge, by = "id"
+  )
+  
+  spu$run <- c14_table_id
+  
+  return(spu)
+  
+})
+
+res_total <- res %>% data.table::rbindlist()
+
+res_total %>%
+  tidyr::pivot_wider(names_from = "run", values_from = c("lat", "lon"))
+
+
+
