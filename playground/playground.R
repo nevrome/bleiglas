b9e24066a23d2bbf85978d5a3f6ed7fa3300b0f2
Clemens Schmid <clemens@nevrome.de>
2020-05-18 20:54:44 +0200

    compiled a playground script to establish the whole workflow

--- playground/playground.R
+++ playground/playground.R
@@ -1,4 +1,58 @@
-hu <- create_pred_grid(polygon_edges) %>% dplyr::filter(
+library(magrittr)
+library(ggplot2)
+
+# download raw data
+c14_cmr <- c14bazAAR::get_c14data("adrac") %>% 
+  # filter data
+  dplyr::filter(!is.na(lat) & !is.na(lon), c14age > 1000, c14age < 3000, country == "CMR") 
+
+# remove doubles
+c14_cmr_unique <- c14_cmr %>%
+  dplyr::mutate(
+    rounded_coords_lat = round(lat, 3),
+    rounded_coords_lon = round(lon, 3)
+  ) %>%
+  dplyr::group_by(rounded_coords_lat, rounded_coords_lon, c14age) %>%
+  dplyr::filter(dplyr::row_number() == 1) %>%
+  dplyr::ungroup()
+
+# transform coordinates
+coords <- data.frame(c14_cmr_unique$lon, c14_cmr_unique$lat) %>% 
+  sf::st_as_sf(coords = c(1, 2), crs = 4326) %>% 
+  sf::st_transform(crs = 4088) %>% 
+  sf::st_coordinates()
+
+# create active dataset
+c14 <- c14_cmr_unique %>% 
+  dplyr::transmute(
+    id = 1:nrow(.),
+    x = coords[,1], 
+    y = coords[,2], 
+    z = c14age * 1000, # rescaling of temporal data
+    material = material
+  )
+
+
+
+raw_voro_output <- bleiglas::tessellate(
+  c14[, c("id", "x", "y", "z")],
+  x_min = min(c14$x) - 150000, x_max = max(c14$x) + 150000, 
+  y_min = min(c14$y) - 150000, y_max = max(c14$y) + 150000
+)
+
+
+polygon_edges <- bleiglas::read_polygon_edges(raw_voro_output)
+
+polygon_edges %<>% dplyr::mutate(
+  z.a = z.a / 1000,
+  z.b = z.b / 1000
+)
+
+c14 %<>% dplyr::mutate(
+  z = z / 1000
+)
+
+hu <- bleiglas::create_pred_grid(polygon_edges) %>% dplyr::filter(
   z == 1011
 )
 
@@ -7,9 +61,4 @@ ggplot() +
     data = hu,
     mapping = aes(x, y, color = as.character(id)),
     alpha = 0.5, size = 10
-  ) +
-  geom_point(
-    data = do.call(rbind, polygons_2D[[1]]),
-    mapping = aes(x, y, color = as.character(id)),
-    shape = 18, size = 10, alpha = 0.5
   )
