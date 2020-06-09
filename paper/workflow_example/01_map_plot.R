02a1ad5d06e1a4a51a525dfbb30263a4e521cc70
Clemens Schmid <clemens@nevrome.de>
2020-06-09 13:29:39 +0200

    simplified and adjusted map plot for this purpose

--- paper/workflow_example/01_map_plot.R
+++ paper/workflow_example/01_map_plot.R
@@ -1,23 +1,9 @@
 library(magrittr)
 library(ggplot2)
 
-load("analysis/data/tmp_data/dates_prepared.RData")
-load("analysis/data/tmp_data/research_area.RData")
-load("analysis/data/tmp_data/extended_area.RData")
-
-dates_prepared_unnested <- dates_prepared %>% 
-  dplyr::select(labnr, calage_density_distribution, burial_type, burial_construction, geometry) %>%
-  tidyr::unnest("calage_density_distribution")
-
-dates_slices <- dates_prepared_unnested <- dates_prepared_unnested %>%
-  dplyr::filter(
-    age %in% seq(-2200, -800, by = 200)
-  ) %>%
-  dplyr::mutate(
-    age_slice = factor(age, levels = seq(-2200, -800, by = 200))
-  )
-
-dates_slices_geo <- dates_slices %>% sf::st_as_sf()
+load("inst/workflow_example/dates_prepared.RData")
+load("inst/workflow_example/research_area.RData")
+load("inst/workflow_example/extended_area.RData")
 
 ex <- raster::extent(research_area)
 xlimit <- c(ex[1], ex[2])
@@ -32,15 +18,15 @@ hu <- ggplot() +
     data = research_area,
     fill = NA, colour = "black", size = 0.5
   ) +
-  geom_sf(
-    data = dates_slices_geo,
+  geom_point(
+    data = dates_prepared,
     mapping = aes(
+      x = x,
+      y = y,
       color = burial_type,
       shape = burial_construction,
-      size = burial_construction,
-      alpha = norm_dens
-    ),
-    show.legend = "point"
+      size = burial_construction
+    )
   ) +
   theme_bw() +
   coord_sf(
@@ -71,33 +57,28 @@ hu <- ggplot() +
     )
   ) +
   theme(
-    plot.title = element_text(size = 30, face = "bold"),
     legend.position = "bottom",
-    legend.title = element_text(size = 20, face = "bold"),
+    legend.title = element_text(size = 19, face = "bold"),
     axis.title = element_blank(),
-    axis.text = element_blank(),
-    legend.text = element_text(size = 20),
-    panel.grid.major = element_line(colour = "grey", size = 0.3),
-    strip.text.x = element_text(size = 20)
+    axis.text = element_text(size = 20),
+    legend.text = element_text(size = 19),
+    panel.background = element_rect(fill = "#BFD5E3")
   ) +
   guides(
     color = guide_legend(title = "Burial type", override.aes = list(size = 10), nrow = 2, byrow = TRUE, order = 1),
     shape = guide_legend(title = "Burial construction", override.aes = list(size = 10), nrow = 2, byrow = TRUE, order = 2),
     size = FALSE,
     alpha = guide_legend(title = "Calibration density", override.aes = list(size = 10, shape = 15), nrow = 2, byrow = TRUE, order = 3)
-  ) +
-  facet_wrap(
-    nrow = 2,
-    ~age_slice
   )
 
 hu %>%
   ggsave(
-    "analysis/figures/map_timeslices.jpeg",
+    "paper/01_map_plot.jpeg",
     plot = .,
     device = "jpeg",
     scale = 1,
     dpi = 300,
-    width = 550, height = 280, units = "mm",
+    width = 300, height = 280, units = "mm",
     limitsize = F
   )
+
