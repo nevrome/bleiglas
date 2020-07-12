1e47b7a5eca8ce8be53c39249fc0e90855767448
Clemens Schmid <clemens@nevrome.de>
2020-07-12 22:55:12 +0200

    and more

--- inst/workflow_example/05_3D_plot.R
+++ inst/workflow_example/05_3D_plot.R
@@ -2,22 +2,12 @@ library(magrittr)
 
 load("inst/workflow_example/tesselation_calage_center_burial_type.RData")
 load("inst/workflow_example/research_area.RData")
-raps <- sf::st_coordinates(sf::st_cast(research_area, "POINT"))[1:4,]/1000
 
-#### 3d plot rgl #### 
-# rgl::axes3d()
-# rgl::points3d(vertices$x, vertices$y, vertices$z, color = "red")
-# rgl::aspect3d(1, 1, 1)
-# rgl::segments3d(
-#   x = as.vector(t(polygon_edges[,c(1,4)])),
-#   y = as.vector(t(polygon_edges[,c(2,5)])),
-#   z = as.vector(t(polygon_edges[,c(3,6)]))
-# )
-
-# rgl::writeWebGL(filename = "plots/tesselation.html",  width = 700, height = 700)
+#### prepare vertex data ####
 
 threed <- vertices %>%
   dplyr::transmute(
+    # scale to kilometres from meters
     x = x/1000, 
     y = y/1000,
     z = z
@@ -28,9 +18,14 @@ polygon_edges$y.a <- polygon_edges$y.a/1000
 polygon_edges$x.b <- polygon_edges$x.b/1000
 polygon_edges$y.b <- polygon_edges$y.b/1000
 
-# plot
-jpeg(filename = "paper/05_3D_plot.jpeg", width = 22, height = 14, units = "cm", res = 300)
+#### 3D plot construction ####
 
+jpeg(
+  filename = "paper/05_3D_plot.jpeg", 
+  width = 22, height = 14, units = "cm", res = 300
+)
+
+# vertex points
 s <- scatterplot3d::scatterplot3d(
   threed$x, threed$y, threed$z, color = "red",
   cex.symbols = 0.7, angle = 70, pch = 18,
@@ -39,14 +34,27 @@ s <- scatterplot3d::scatterplot3d(
   xlim = c(-1700, 1200), ylim = c(1000, 3500), zlim = c(-2300, -700)
 )
 
-#### tesselation ####
-csstart <- s$xyz.convert(polygon_edges[[1]], polygon_edges[[2]], polygon_edges[[3]])
-csstop <- s$xyz.convert(polygon_edges[[4]], polygon_edges[[5]], polygon_edges[[6]])
+# tessellation polygon edges
+csstart <- s$xyz.convert(
+  polygon_edges[[1]], 
+  polygon_edges[[2]], 
+  polygon_edges[[3]]
+)
+csstop <- s$xyz.convert(
+  polygon_edges[[4]], 
+  polygon_edges[[5]], 
+  polygon_edges[[6]]
+)
 for(i in 1:length(csstart$x)) {
-  segments(csstart$x[i], csstart$y[i], csstop$x[i], csstop$y[i], lwd = 0.1, col = "black")
+  segments(
+    csstart$x[i], csstart$y[i], csstop$x[i], csstop$y[i], 
+    lwd = 0.1, col = "black"
+  )
 }
 
-#### research area box ####
+# research area box
+raps <- sf::st_coordinates(sf::st_cast(research_area, "POINT"))[1:4,]/1000
+
 a1 <- s$xyz.convert(raps[1,1], raps[1,2], -2200)
 a2 <- s$xyz.convert(raps[2,1], raps[2,2], -2200)
 a3 <- s$xyz.convert(raps[3,1], raps[3,2], -2200)
