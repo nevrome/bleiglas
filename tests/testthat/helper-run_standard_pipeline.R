3d8a5f3e564d9778956bc0d6c6f31feaf30a3d8c
Clemens Schmid <clemens@nevrome.de>
2020-07-13 22:56:25 +0200

    more complete check pipeline

--- tests/testthat/helper-run_standard_pipeline.R
+++ tests/testthat/helper-run_standard_pipeline.R
@@ -1,17 +1,33 @@
 # This code runs before the tests.
 
-random_unique_points <- unique(data.table::data.table(
-  id = NA,
-  x = runif(10),
-  y = runif(10),
-  z = runif(10)
-))
-random_unique_points$id <- 1:10
+test_data_set <- lapply(1:5, function(i) {
+  current_iteration <- data.table::data.table(
+    id = 1:5,
+    x = c(1, 2, 3, 2, 1) + rnorm(5, 0, 0.1),
+    y = c(3, 1, 4, 4, 3) + rnorm(5, 0, 0.1),
+    z = c(1, 3, 4, 2, 5) + rnorm(5, 0, 0.1),
+    value1 = c("Brot", "Kaese", "Wurst", "Gurke", "Brot"),
+    value2 = c(5.3, 5.1, 5.8, 1.0, 1.2)
+  )
+  data.table::setkey(current_iteration, "x", "y", "z")
+  unique( current_iteration )
+})
 
-voro_output <- tessellate(random_unique_points)
+test_data <- test_data_set[[1]]
+
+voro_output <- tessellate(test_data[, c("id", "x", "y", "z")])
 
 polygon_points <- read_polygon_edges(voro_output)
 
-cut_surfaces <- cut_polygons(polygon_points, c(0.2, 0.4, 0.6))
+cut_surfaces <- cut_polygons(polygon_points, c(2, 3, 4))
 
 cut_surfaces_sf <- cut_polygons_to_sf(cut_surfaces, crs = 25832)
+
+all_iterations <- data.table::rbindlist(test_data_set)
+prediction_grid <- expand.grid(
+  x = seq(min(all_iterations$x), max(all_iterations$x), length.out = 10),
+  y = seq(min(all_iterations$y), max(all_iterations$y), length.out = 10),
+  z = seq(min(all_iterations$z), max(all_iterations$z), length.out = 5)
+)
+
+bleiglas::predict_grid(test_data_set, prediction_grid, cl = 1)
