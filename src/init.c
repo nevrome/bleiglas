1d91d5353a7d66c8e677818ef57f224275b0b610
Clemens Schmid <clemens@nevrome.de>
2020-10-11 16:44:55 +0200

    further work on the voro++ wrapping

--- src/init.c
+++ src/init.c
@@ -14,12 +14,14 @@ extern SEXP _bleiglas_line_segment_plane_intersection(SEXP, SEXP, SEXP, SEXP);
 extern SEXP _bleiglas_line_segment_plane_intersection_multi(SEXP, SEXP, SEXP);
 extern SEXP _bleiglas_pnp(SEXP, SEXP, SEXP, SEXP);
 extern SEXP _bleiglas_pnpmulti(SEXP, SEXP, SEXP);
+extern SEXP _bleiglas_voropp_Rcpp_interface(SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
   {"_bleiglas_line_segment_plane_intersection",       (DL_FUNC) &_bleiglas_line_segment_plane_intersection,       4},
   {"_bleiglas_line_segment_plane_intersection_multi", (DL_FUNC) &_bleiglas_line_segment_plane_intersection_multi, 3},
   {"_bleiglas_pnp",                                   (DL_FUNC) &_bleiglas_pnp,                                   4},
   {"_bleiglas_pnpmulti",                              (DL_FUNC) &_bleiglas_pnpmulti,                              3},
+  {"_bleiglas_voropp_Rcpp_interface",                 (DL_FUNC) &_bleiglas_voropp_Rcpp_interface,                 1},
   {NULL, NULL, 0}
 };
 
--- src/voropp_Rcpp_interface.cpp
+++ src/voropp_Rcpp_interface.cpp
@@ -1,13 +1,39 @@
 #include <Rcpp.h>
 #include <cstring>
-#include "voro++.h"
+#include "voro++0.4.6/voro++.h"
 using namespace voro;
 using namespace Rcpp;
+
+// This function returns a random floating point number between 0 and 1
+double rnd() {return double(rand())/RAND_MAX;}
 
 //' voro++ interface
 //' @noRd
 // [[Rcpp::export]]
 SEXP voropp_Rcpp_interface(NumericVector point_a) {
+  
+  double x,y,z,rsq,r;
+  voronoicell v;
+        
+  // Initialize the Voronoi cell to be a cube of side length 2, centered
+  // on the origin
+  v.init(-1,1,-1,1,-1,1);
+  
+  // Cut the cell by 250 random planes which are all a distance 1 away
+  // from the origin, to make an approximation to a sphere
+  for(int i=0;i<250;i++) {
+           x=2*rnd()-1;
+           y=2*rnd()-1;
+           z=2*rnd()-1;
+           rsq=x*x+y*y+z*z;
+           if(rsq>0.01&&rsq<1) {
+                     r=1/sqrt(rsq);x*=r;y*=r;z*=r;
+                     v.plane(x,y,z,1);
+             }
+   }
+  
+   // Output the Voronoi cell to a file, in the gnuplot format
+  v.draw_gnuplot(0,0,0,"single_cell.gnu");
   
   int i = 1;
   
